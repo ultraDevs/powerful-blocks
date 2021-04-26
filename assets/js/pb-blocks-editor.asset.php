c3952622dda03b2741a472e84e5f257a6ffbab90
mhimon <mahbubhasanimon@gmail.com>
2021-04-26 08:51:46 +0600

    Updated Fun Factor Block

--- assets/js/pb-blocks-editor.asset.php
+++ assets/js/pb-blocks-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'bc877c48b23610252640702671392964');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'fe28d79d54ca1c30a2887d2a54654f8a');
